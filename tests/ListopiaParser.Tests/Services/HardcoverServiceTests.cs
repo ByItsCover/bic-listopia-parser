af443dc7f5f9e0f17747701b3ac93b0a9a1fa3bf
Kalamojo <kojo22444488888888@gmail.com>
2026-05-31 16:06:16 -0400

    Testing failed test

--- tests/ListopiaParser.Tests/Services/HardcoverServiceTests.cs
+++ tests/ListopiaParser.Tests/Services/HardcoverServiceTests.cs
@@ -59,7 +59,6 @@ public class HardcoverServiceTests
                             editions(where: { isbn_13: { _in: $isbn_list } }) {
                                 id
                                 isbn_13
-                                book_id
                                 image {
                                     url
                                 }
