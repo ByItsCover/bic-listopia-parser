d199f9cbf5fa3068dbcd17b7ba054b56f111a52f
Kalamojo <kojo22444488888888@gmail.com>
2026-05-31 15:29:17 -0400

    Adding scraping header

--- src/ListopiaParser/Services/ListopiaService.cs
+++ src/ListopiaParser/Services/ListopiaService.cs
@@ -16,6 +16,8 @@ public class ListopiaService : IListopiaService
 
     public ListopiaService(HttpClient client, IOptions<ListopiaOptions> options, ILogger<ListopiaService> logger)
     {
+        client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36");
+        
         _client = client;
         _options = options.Value;
         _logger = logger;
